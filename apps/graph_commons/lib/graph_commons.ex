3bc5bdeb7350e4290fd2eb9ce1597ccb09acc722
Carlo Gilmar <carlogilmar12@gmail.com>
2020-05-17 02:08:44 -0500

    5. Adding definitions for get the priv directories

--- apps/graph_commons/lib/graph_commons.ex
+++ apps/graph_commons/lib/graph_commons.ex
@@ -1,18 +1,14 @@
 defmodule GraphCommons do
-  @moduledoc """
-  Documentation for `GraphCommons`.
-  """
+  @typedoc "Types for graph storage."
+  @type base_type :: :native | :property | :rdf | :tinker
+  @type graph_type :: base_type()
+  @type query_type :: base_type()
+  @typedoc "Type for testing file types."
+  @type file_test :: :dir? | :regular? | :exists?
+  @priv_dir "#{:code.priv_dir(:graph_commons)}"
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> GraphCommons.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  def exports_dir(), do: @priv_dir <> "/transfer/exports"
+  def imports_dir(), do: @priv_dir <> "/transfer/imports"
+  def scripts_dir(), do: @priv_dir <> "/extras/scripts"
+  def storage_dir(), do: @priv_dir <> "/storage"
 end
