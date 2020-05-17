54dbf646d45e65f7a4a9e8238315ec237431a432
Carlo Gilmar <carlogilmar12@gmail.com>
2020-05-17 01:33:47 -0500

    1. Adding app graph_common and implement struct Graph

---
+++ apps/graph_commons/lib/graph_commons/graph.ex
@@ -0,0 +1,31 @@
+defmodule GraphCommons.Graphs do
+  @enforce_keys ~w[data file type]a
+  defstruct ~w[data file path type uri]a
+
+	@type graph_data :: String.t()
+	@type graph_file :: String.t()
+	@type graph_path :: String.t()
+	@type graph_type :: GraphCommons.graph_type()
+	@type graph_uri :: String.t()
+	@type t :: %__MODULE__{
+		data: graph_data,
+		file: graph_file,
+		type: graph_type,
+		path: graph_path,
+		uri: graph_uri }
+
+	defguard is_graph_type(graph_type)
+	when graph_type == :native or graph_type == :property or
+	graph_type == :rdf or graph_type == :tinker
+
+	@spec new(graph_data, graph_file, graph_type) :: t
+	def new(graph_data, graph_file, graph_type) when is_graph_type(graph_type) do
+		graph_path = "#{@storage_dir}/#{graph_type}/graphs/#{graph_file}"
+
+		%__MODULE__{
+			data: graph_data, file: graph_file, type: graph_type, # system
+			path: graph_path,
+			uri: "file://" <> graph_path
+		}
+	end
+end
---
+++ apps/graph_commons/mix.exs
@@ -0,0 +1,32 @@
+defmodule GraphCommons.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :graph_commons,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.10",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ex_doc, "~> 0.20", only: :dev, runtime: false}
+      # {:sibling_app_in_umbrella, in_umbrella: true}
+    ]
+  end
+end
