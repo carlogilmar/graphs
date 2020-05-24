662354f2c9f54b9479d7cd90893df987ab0e0ca9
Carlo Gilmar <carlogilmar12@gmail.com>
2020-05-24 01:09:21 -0500

    14. Removing innecesary functions from native_graph

--- apps/native_graph/lib/native_graph.ex
+++ apps/native_graph/lib/native_graph.ex
@@ -1,8 +1,4 @@
 defmodule NativeGraph do
   use GraphCommons.Graph, graph_type: :native, graph_module: __MODULE__
 
-  def read_graph(graph_file), do: GraphCommons.Graph.read_graph(graph_file, :native)
-
-  def write_graph(graph_data, graph_file),
-    do: GraphCommons.Graph.write_graph(graph_data, graph_file, :native)
 end
