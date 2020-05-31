5476d14833a94e5b2816ab27cf1b6efb372b91f3
Carlo Gilmar <carlogilmar12@gmail.com>
2020-05-30 23:34:04 -0500

    17. Adding delegate functions in NativeGraph

--- apps/native_graph/lib/native_graph.ex
+++ apps/native_graph/lib/native_graph.ex
@@ -1,4 +1,6 @@
 defmodule NativeGraph do
   use GraphCommons.Graph, graph_type: :native, graph_module: __MODULE__
 
+  defdelegate write_image(arg), to: NativeGraph.Format, as: :to_png
+  defdelegate write_image(arg1, arg2), to: NativeGraph.Format, as: :to_png
 end
