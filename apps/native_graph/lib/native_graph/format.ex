8fd1eaefffb4ff622a1f6e1d013ccfb283e470a5
Carlo Gilmar <carlogilmar12@gmail.com>
2020-05-30 23:53:20 -0500

    Fixing error in line

--- apps/native_graph/lib/native_graph/format.ex
+++ apps/native_graph/lib/native_graph/format.ex
@@ -5,7 +5,8 @@ defmodule Format do
 
 	@spec to_png(GraphCommons.Graph.t(), layout) :: String.t()
 	def to_png(%GraphCommons.Graph{} = graph, layout \\ :dot) do
-		layout_cmd = Path.join(@binary_dir, Atom.to_string(layout)) dot_file = graph.path
+		layout_cmd = Path.join(@binary_dir, Atom.to_string(layout))
+		dot_file = graph.path
 		png_file = @graph_images_dir <> Path.basename(dot_file, ".dot") <> ".png"
 		with {_, 0} <-
 			System.cmd(layout_cmd, ["-T", "png", dot_file, "-o", png_file])
