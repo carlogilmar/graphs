4f3d7c99ef75a830747481c46b9bccef2358b901
Carlo Gilmar <carlogilmar12@gmail.com>
2020-05-31 00:30:25 -0500

    29. Adding to_query_to_graph and guard

--- apps/graph_commons/lib/graph_commons/utils.ex
+++ apps/graph_commons/lib/graph_commons/utils.ex
@@ -7,4 +7,14 @@ defmodule GraphCommons.Utils do
   end
 
   def libgraph!(query_string), do: to_query_graph!(NativeGraph, query_string)
+
+  defguard is_module(graph_module)
+  when graph_module == DGraph or graph_module == NativeGraph or
+  graph_module == PropertyGraph or graph_module == RDFGraph or graph_module == TinkerGraph
+
+  defp to_query_graph!(graph_module, query_string) when is_module(graph_module) do
+    query_string
+    |> graph_module.new_query()
+    |> graph_module.query_graph!()
+  end
 end
